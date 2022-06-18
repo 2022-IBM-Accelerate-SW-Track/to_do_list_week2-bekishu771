50288226022de0d43734aac44168a7e9f4eaf809
Bereket Tenaye <tenay002@umn.edu>
2022-06-18 13:55:57 -0500

    Finished working on week 2 assignment tasks

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import prof from "../assets/Pic_404.jpg";  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
-        </div>
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            src={prof}
+            alt="Profile Pic"
+          ></img>
       </div>
-    );
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title" class="mya_text">   Bereket Tenaye</div>
+        <div className="brief_description" class="my_text">
+           Hello there! I am a rising senior studying electrical engineering at the 
+          University of Minnesota-Twin Cities. I live in Saint Paul, Minnesota.
+          </div>
+      </div>
+    </div>
+  </div>
+    )
   }
 }
+
+          
+       
